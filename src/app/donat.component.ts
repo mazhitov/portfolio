c25079cb3679f7eeec051cb448c82a89d493a028
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-12-02 10:40:55 +0600

    add donat component

---
+++ src/app/donat.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donat',
+  template: `<div class="container">
+    <h3 class="btn-success p-2">Successful!</h3>
+    <div class="card-body">
+      <p>Thank you for donat!</p>
+      <p>We will call back soon</p>
+    </div>
+  </div>`
+})
+
+export class DonatComponent {
+
+}
